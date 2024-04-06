d5861a51a5101d6e59d845ff71a51a52b77ee64c
KIMMIJU <hviezda1212@gmail.com>
2024-04-07 00:01:54 +0900

    Route 구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
 
 function App() {
   return (
-    <div className="App">
-      <AppLayout></AppLayout>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="movies">
+        <Route index element={<MoviePage />} />
+        <Route path=":id" element={<MovieDetailPage />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
