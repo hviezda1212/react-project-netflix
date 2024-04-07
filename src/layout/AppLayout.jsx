90f9d4b41b62ed108677b96b514bb0bb106807e5
KIMMIJU <hviezda1212@gmail.com>
2024-04-07 22:03:39 +0900

    logo 변경

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -5,7 +5,7 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Outlet, Link } from "react-router-dom";
-import logoImg from "../assets/images/netflix-logo.png";
+import logoImg from "../assets/images/logo.svg";
 
 const AppLayout = () => {
   return (
