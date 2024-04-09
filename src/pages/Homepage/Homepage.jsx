04ce1cce92c3733ed321a8598819c6222dee3d81
KIMMIJU <hviezda1212@gmail.com>
2024-04-09 21:13:01 +0900

    movie slide 추가

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from "./components/Banner/Banner"
-import PopularMovieSlide from './components/PopularMovieSlide/PopularMovieSlide'
+import PopularMovieSlide from './components/MovieSlide/PopularMovieSlide'
+import TopRatedMovieSlide from './components/MovieSlide/TopRatedMovieSlide'
+import UpcomingMovieSlide from './components/MovieSlide/UpcomingMovieSlide'
 
 const Homepage = () => {
   return (
     <div>
       <Banner/>
       <PopularMovieSlide/>
+      <TopRatedMovieSlide/>
+      <UpcomingMovieSlide/>
     </div>
   )
 }
---
+++ src/pages/Homepage/components/MovieSlide/UpcomingMovieSlide.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useUpcomingMoviesQuery } from "../../../../hooks/useUpcomingMovies";
+import { Alert } from "bootstrap";
+import "react-multi-carousel/lib/styles.css";
+import MovieSlider from "../../../../common/MovieSlider/MovieSlider";
+import { responsive } from "../../../../constants/responsive";
+
+const UpcomingMovieSlide = () => {
+  const { data, isLoading, isError, error } = useUpcomingMoviesQuery();
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return (
+    <div>
+      <MovieSlider
+        title="Upcoming Movies"
+        movies={data.results}
+        responsive={responsive}
+      />
+    </div>
+  );
+};
+
+export default UpcomingMovieSlide;
