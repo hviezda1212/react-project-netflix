a3c7668db4d0eae42117b3edd71c8d902e8233ef
KIMMIJU <hviezda1212@gmail.com>
2024-04-10 23:46:07 +0900

    search

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
 
 const AppLayout = () => {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+  const searchByKeyword = (event) => {
+    event.preventDefault();
+    navigate(`/movies?q=${keyword}`);
+    setKeyword("");
+
+  };
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -21,15 +29,21 @@ const AppLayout = () => {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-            <Link to="/" className="navbar-link">Home</Link>
-            <Link to="/Movies" className="navbar-link">Movies</Link>
+              <Link to="/" className="navbar-link">
+                Home
+              </Link>
+              <Link to="/Movies" className="navbar-link">
+                Movies
+              </Link>
             </Nav>
-            <Form className="d-flex">
+            <Form className="d-flex" onSubmit={searchByKeyword}>
               <Form.Control
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
+                value={keyword}
+                onChange={(event) => setKeyword(event.target.value)}
               />
               <Button variant="outline-danger">Search</Button>
             </Form>
