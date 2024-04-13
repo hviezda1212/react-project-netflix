74e48390d3be66e1f6f549e9b3e648f3ad0f3bd6
KIMMIJU <hviezda1212@gmail.com>
2024-04-14 01:00:04 +0900

    추천영화 수정

--- src/pages/MovieDetail/MovieRecommendation/MovieRecommendation.jsx
+++ src/pages/MovieDetail/MovieRecommendation/MovieRecommendation.jsx
@@ -1,45 +1,48 @@
-import React from 'react';
-import '../MovieRecommendation/MovieRecommendation.style.css';
-import { useMovieRecommend } from '../../../hooks/useMovieRecommend';
-import { useParams } from 'react-router-dom';
-import MovieCard from '../../../common/MovieCard/MovieCard';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import { responsive } from '../../../constants/responsive';
-import isLoadingSpinner from '../../../common/Spinner/LoadingSpinner';
+import React from "react";
+import "../MovieRecommendation/MovieRecommendation.style.css";
+import { useMovieRecommend } from "../../../hooks/useMovieRecommend";
+import { useParams } from "react-router-dom";
+import MovieCard from "../../../common/MovieCard/MovieCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { responsive } from "../../../constants/responsive";
+import isLoadingSpinner from "../../../common/Spinner/LoadingSpinner";
 
 const MovieRecommendation = () => {
-    let params = useParams();
-    const { data, isLoading, isError, error } = useMovieRecommend(params);
+  let params = useParams();
+  const { data, isLoading, isError, error } = useMovieRecommend(params);
 
-    if (isLoading) {
-        return <div>{isLoadingSpinner()}</div>
-    }
-    if (isError) {
-        return <div>{error.message}</div>
-    }
+  if (isLoading) {
+    return <div>{isLoadingSpinner()}</div>;
+  }
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
 
-    let movies = data?.results;
+  let movies = data?.results;
 
-    return (
-        <div className="movie-recommendation-container">
-            <h4 style={{ color: 'white' }}>관련 추천 영화</h4>
-            {movies && movies.length > 0 ? (
-                <Carousel
-                    responsive={responsive}
-                    infinite={true}
-                    containerClass="carousel-container"
-                    itemClass="movie-card"
-                >
-                    {movies.map((movie, index) => (
-                        <MovieCard key={index} movie={movie} />
-                    ))}
-                </Carousel>
-            ) : (
-                <div className="NotFound" style={{ color: 'white' }}>추천 영화가 존재하지 않습니다.</div>
-            )}
+  return (
+    <div className="movie-recommendation-container">
+      <h4 style={{ color: "white" }}>Recommended movies</h4>
+      {movies && movies.length > 0 ? (
+        <Carousel
+          infinite={true}
+          centerMode={true}
+          itemClass="movie-slider p-1"
+          containerClass="carousel-container"
+          responsive={responsive}
+        >
+          {movies.map((movie, index) => (
+            <MovieCard movie={movie} key={index} />
+          ))}
+        </Carousel>
+      ) : (
+        <div className="NotFound" style={{ color: "white" }}>
+          There are no recommended movies.
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default MovieRecommendation;
--- src/pages/MovieDetail/MovieReviews/MovieReviews.jsx
+++ src/pages/MovieDetail/MovieReviews/MovieReviews.jsx
@@ -6,16 +6,15 @@ import { Container, Card, Button } from 'react-bootstrap';
 const MovieReviews = () => {
     let params = useParams();
     const { data } = useMovieReviews(params);
-    const [visibleReviews, setVisibleReviews] = useState(3); // 초기에 보여지는 리뷰의 수를 설정
+    const [visibleReviews, setVisibleReviews] = useState(3);
 
     const handleShowMoreReviews = () => {
-        // 더보기 버튼을 클릭할 때마다 보여지는 리뷰의 수를 증가시킵니다.
-        setVisibleReviews(visibleReviews + 3); // 예를 들어, 3개씩 추가적으로 보여줍니다.
+        setVisibleReviews(visibleReviews + 3);
     };
 
     return (
         <Container>
-            <h4 style={{ color: 'white' }}>리뷰</h4>
+            <h4 style={{ color: 'white' }}>Reviews</h4>
             {data && data.results && data.results.length > 0 ? (
                 data.results.slice(0, visibleReviews).map((review, index) => (
                     <Card key={index} className="my-3" style={{ backgroundColor: '#f8f9fa' }}>
@@ -26,12 +25,11 @@ const MovieReviews = () => {
                     </Card>
                 ))
             ) : (
-                <div className='NotFound' style={{ color: 'white', paddingTop:'50px' }}>리뷰가 존재하지 않습니다.</div>
+                <div className='NotFound' style={{ color: 'white', paddingTop:'50px' }}>There is no review.</div>
             )}
-            {/* 더보기 버튼 */}
             {visibleReviews < (data?.results?.length || 0) && (
                 <div className="text-center mt-4">
-                    <Button variant="danger" onClick={handleShowMoreReviews}>더 많은 리뷰</Button>
+                    <Button variant="danger" onClick={handleShowMoreReviews}>More +</Button>
                 </div>
             )}
         </Container>
